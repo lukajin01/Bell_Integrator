b265eb9a13d9317d88358192d927b287dde08179
Luka <dzhincharadze-luka@mail.ru>
2025-05-13 13:52:43 +0300

    Creating plug

---
+++ src/test/java/com/example/stub_app/StubAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stub_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StubAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
